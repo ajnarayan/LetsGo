3db96f7687bb98aef9a3d93f4e920fcdf60cc4e9
ajnarayan <anjjan.narayan@yahoo.in>
2018-01-15 20:34:28 -0500

    MINOR: Strucutre/format

--- strings/caesarcipher.go
+++ strings/caesarcipher.go
@@ -3,31 +3,43 @@ package strings
 import "fmt"
 import "strings"
 
-func EncodeCharacters(input string, shift int)string{
+func EncodeCharacters(input string, shift int) string {
 	output := []rune(strings.ToLower(input))
 	fmt.Printf("The input string is %s\n", string(output))
 	shiftchar := rune(shift)
 
-	for i := range output{
+	for i := range output {
 		//fmt.Printf("%#U", output[i])
-		output[i] = output[i] + shiftchar
-		if output[i] > rune('z'){
-			output[i] = output[i] - rune(26)
-		}
-	}
+		if output[i] != rune(' ') {
 
+			output[i] = output[i] + shiftchar
+			if output[i] > rune('z') {
+				output[i] = output[i] - rune(26)
+			} else if output[i] < rune('a') {
+				output[i] = output[i] + rune(26)
+			}
+		}
+
+	}
 	//fmt.Printf("%U", rune(26))
+	//fmt.Printf("%U",rune('z'))
 	fmt.Printf("The Encrypted String is : %s \n", string(output))
 	return string(output)
 }
 
-func DecodeCharacters(input string, shift int){
+func DecodeCharacters(input string, shift int) {
 	output := []rune(strings.ToLower(input))
 	shiftchar := rune(shift)
-	for i := range output{
-		output[i] = output[i] - shiftchar
-
+	for i := range output {
+		if output[i] != rune(' ') {
+			output[i] = output[i] - shiftchar
+			if output[i] > rune('z') {
+				output[i] = output[i] - rune(26)
+			} else if output[i] < rune('a') {
+				output[i] = output[i] + rune(26)
+			}
+		}
 	}
 
-	fmt.Printf("The Decrypted value is %s \n",string(output))
+	fmt.Printf("The Decrypted value is %s \n", string(output))
 }
